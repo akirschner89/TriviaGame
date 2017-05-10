ba984a094707cfc025ae472779362c8f7d610388
Adam Kirschner <akirschner_atx@Adams-MacBook-Air.local>
2017-05-09 22:33:19 -0500

    Styles docs

---
+++ assets/javascript/app.js
@@ -0,0 +1,74 @@
+// build objects to hold questions & their answers
+
+// use a questionCounter to keep track of the question a user is on
+
+// objects, arrays, variables
+// ------------------------------------------------------------------------------
+var correct = 0;
+var wrong = 0;
+var unanswered = 0;
+var timerCount = 30;
+var questions = {
+	"questionOne": {
+			"question": "What pick was Manu Ginolbili in the 1999 draft?",
+			"answer1": "57",
+			"answer2": "59",
+			"answer3": "41",
+			"answer4": "48",
+			"gif": "../images/ginobili_giphy.mp4",
+		},
+
+		"questionTwo": {
+			"question": "Who has the highest scoring average in Spurs history?",
+			"answer1": "Tim Duncan",
+			"answer2": "George Gervin",
+			"answer3": "David Robinson",
+			"answer4": "Kahwi Leonard",
+			"gif": "../images/george_giphy.mp4",
+		},
+		"questionThree": {
+					"question": "What year was the Spurs franchise founded?",
+					"answer1": "1971",
+					"answer2": "1978",
+					"answer3": "1967",
+					"answer4": "1966",
+					"gif": "../images/cayote_giphy.mp4",
+		},
+		"questionFour": {
+					"question": "What place is Timmy on the All-Time NBA Blocks List?",
+					"answer1": "4th",
+					"answer2": "7th",
+					"answer3": "6th",
+					"answer4": "5th",
+					"gif": "../images/duncan_giphy.mp4",
+		},
+		"questionFive": {
+					"question": "Where was Coach Pop's first head coaching job?",
+					"answer1": "Pomona College",
+					"answer2": "UMass",
+					"answer3": "Pepperdine University",
+					"answer4": "SMU",
+					"gif": "../images/pop_giphy.mp4",
+		},
+
+};
+
+
+
+
+
+
+
+
+// functions
+// ------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+// process
+// ------------------------------------------------------------------------------

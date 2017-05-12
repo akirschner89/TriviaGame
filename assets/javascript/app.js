e05ccb635497b84c97b8d4e75254c1c74d9e9a81
Adam Kirschner <akirschner_atx@Adams-MacBook-Air.local>
2017-05-11 23:01:01 -0500

    Phase III

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,99 +1,117 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
-// objects, arrays, variables
-// ------------------------------------------------------------------------------
-var correct = 0;
-var wrong = 0;
-var unanswered = 0;
-var shotClock = 30;
-var questions = {
+    // objects, arrays, variables
+    // ------------------------------------------------------------------------------
+    var correct = 0;
+    var wrong = 0;
+    var unanswered = 0;
+    var shotClock = 10;
+    var questions = {
 
-		questionOne: {
-			question: "What pick was Manu Ginolbili in the 1999 draft?",
-			answer1: "57",
-			answer2: "59",
-			answer3: "41",
-			answer4: "48",
-			gif: "../images/ginobili_giphy.mp4",
-		},
+        questionOne: {
+            question: "What pick was Manu Ginolbili in the 1999 draft?",
+            answers: ["57", "59", "41", "48"],
+            // answer2: "59",
+            // answer3: "41",
+            // answer4: "48",
+            gif: "assets/images/ginobili_giphy.gif",
+        },
 
-		questionTwo: {
-			question: "Who has the highest scoring average in Spurs history?",
-			answer1: "Tim Duncan",
-			answer2: "George Gervin",
-			answer3: "David Robinson",
-			answer4: "Kahwi Leonard",
-			gif: "../images/george_giphy.mp4",
-		},
-		questionThree: {
-					question: "What year was the Spurs franchise founded?",
-					answer1: "1971",
-					answer2: "1978",
-					answer3: "1967",
-					answer4: "1966",
-					gif: "../images/cayote_giphy.mp4",
-		},
-		questionFour: {
-					question: "What place is Timmy on the All-Time NBA Blocks List?",
-					answer1: "4th",
-					answer2: "7th",
-					answer3: "6th",
-					answer4: "5th",
-					gif: "../images/duncan_giphy.mp4",
-		},
-		questionFive: {
-					question: "Where was Coach Pop's first head coaching job?",
-					answer1: "Pomona College",
-					answer2: "UMass",
-					answer3: "Pepperdine University",
-					answer4: "SMU",
-					gif: "../images/pop_giphy.mp4",
-		},
+        questionTwo: {
+            question: "Who has the highest scoring average in Spurs history?",
+            answers: ["Tim Duncan", "George Gervin", "David Robinson", "Kahwi Leonard"],
+            // answer2: "George Gervin",
+            // answer3: "David Robinson",
+            // answer4: "Kahwi Leonard",
+            gif: "assets/images/george_giphy.gif",
+        },
+        questionThree: {
+            question: "What year was the Spurs franchise founded?",
+            answers: ["1971", "1978", "1967", "1966"],
+            // answer2: "1978",
+            // answer3: "1967",
+            // answer4: "1966",
+            gif: "assets/images/spurs_giphy.gif",
+        },
+        questionFour: {
+            question: "What place is Timmy on the All-Time NBA Blocks List?",
+            answers: ["4th", "7th", "6th", "5th"],
+            // answer2: "7th",
+            // answer3: "6th",
+            // answer4: "5th",
+            gif: "assets/images/duncan_giphy.gif",
+        },
+        questionFive: {
+            question: "Where was Coach Pop's first head coaching job?",
+            answers: ["Pomona College", "UMass", "Pepperdine University", "SMU"],
+            // answer2: "UMass",
+            // answer3: "Pepperdine University",
+            // answer4: "SMU",
+            gif: "assets/images/pop_giphy.gif",
+        },
 
-};
+    };
 
-var button = $("<button>");
-var div  = $("<div>");
-
-
-
-
-
-
-// functions
-// ------------------------------------------------------------------------------
-function setGame() {
-
-	$("#gameContainer").html("<div><p>Shot Clock: " + parseInt(shotClock) + "</p></div>");
-	$("#gameContainer").append("<div><p>" + questions.questionOne.question + "</p></div>");
-	div.addClass("gameTimeQs");
-	div.text(questions.questionOne.answer1);
-	$("#gameContainer").append(div);
-	div.text(questions.questionOne.answer2);
-	$("#gameContainer").append(div);
-	div.text(questions.questionOne.answer3);
-	$("#gameContainer").append(div);
-	div.text(questions.questionOne.answer4);
-	$("#gameContainer").append(div);
-
-};
+    var button = $("<button>");
+    var div = $("<div>");
+    var intervalId;
 
 
 
 
-// main process
-// ------------------------------------------------------------------------------
+
+    // functions
+    // ------------------------------------------------------------------------------
+    function setGame1() {
+
+        intervalId = setInterval(function() {
+            shotClock--;
+            console.log(shotClock);
+            $("#gameContainer").html("<div><p>Shot Clock: " + shotClock + "</p></div>");
+            $("#gameContainer").append("<div><p>" + questions.questionOne.question + "</p></div>");
+
+            for (var i = 0; i < 4; i++) {
+                var button = $("<button>");
+                button.addClass("gameTimeAs");
+                button.attr("data-name", questions.questionOne.answers[i]);
+                button.text(questions.questionOne.answers[i]);
+                $("#gameContainer").append(button);
+                clockCheck();
+
+            }
+
+        }, 1000);
+
+        function clockCheck() {
+            if (shotClock === 0) {
+                unanswered++;
+                clearInterval(intervalId);
+                $("#gameContainer").html("<div><p>Shot Clock: " + shotClock + "</p></div>");
+                $("#gameContainer").append("<div><p>We call that an AIRBALL!!!</p></div>");
+                $("#gameContainer").append("<img src=" + questions.questionOne.gif + ">");
+
+            }
+        };
+
+    };
 
 
-// var button = $("<button>");
-button.addClass("begin");
-button.text("TipOff");
-$("#gameContainer").html(button);
 
-$(".begin").click(function() {
-  $(".begin").hide( "slow" );
-  setGame();
-});
+
+    // main process
+    // ------------------------------------------------------------------------------
+
+
+    // var button = $("<button>");
+    // button.attr("id", "begin");
+    button.addClass("begin");
+    button.text("TipOff");
+    $("#startButton").html(button);
+
+    $(".begin").click(function() {
+        $(".begin").hide("slow");
+        setGame1();
+    });
 
 
 

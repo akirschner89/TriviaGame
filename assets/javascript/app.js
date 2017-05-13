6992b4393d270541dd52355f1b2230221e6fbe86
Adam Kirschner <akirschner_atx@Adams-MacBook-Air.local>
2017-05-12 19:56:35 -0500

    Phase IV

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,117 +6,161 @@ $(document).ready(function() {
     var wrong = 0;
     var unanswered = 0;
     var shotClock = 10;
-    var questions = {
+    var questions = [
 
-        questionOne: {
+        {
             question: "What pick was Manu Ginolbili in the 1999 draft?",
             answers: ["57", "59", "41", "48"],
-            // answer2: "59",
-            // answer3: "41",
-            // answer4: "48",
+            correctAnswer: "57",
             gif: "assets/images/ginobili_giphy.gif",
         },
 
-        questionTwo: {
+        {
             question: "Who has the highest scoring average in Spurs history?",
             answers: ["Tim Duncan", "George Gervin", "David Robinson", "Kahwi Leonard"],
-            // answer2: "George Gervin",
-            // answer3: "David Robinson",
-            // answer4: "Kahwi Leonard",
+            correctAnswer: "George Gervin",
             gif: "assets/images/george_giphy.gif",
-        },
-        questionThree: {
+        }, 
+
+        {
             question: "What year was the Spurs franchise founded?",
             answers: ["1971", "1978", "1967", "1966"],
-            // answer2: "1978",
-            // answer3: "1967",
-            // answer4: "1966",
+            correctAnswer: "1967",
             gif: "assets/images/spurs_giphy.gif",
-        },
-        questionFour: {
+        }, 
+
+        {
             question: "What place is Timmy on the All-Time NBA Blocks List?",
             answers: ["4th", "7th", "6th", "5th"],
-            // answer2: "7th",
-            // answer3: "6th",
-            // answer4: "5th",
+            correctAnswer: "5th",
             gif: "assets/images/duncan_giphy.gif",
-        },
-        questionFive: {
+        }, 
+
+        {
             question: "Where was Coach Pop's first head coaching job?",
             answers: ["Pomona College", "UMass", "Pepperdine University", "SMU"],
-            // answer2: "UMass",
-            // answer3: "Pepperdine University",
-            // answer4: "SMU",
+            correctAnswer: "Pomona College",
             gif: "assets/images/pop_giphy.gif",
         },
 
-    };
+    ];
 
     var button = $("<button>");
     var div = $("<div>");
     var intervalId;
 
-
-
-
+    //    var game = {
+    //    questions: setGame(),
+    //  	correct: 0,
+    //    wrong: 0,
+    //    unanswered: 0,
+    //    // timer: intervalId,
+    //    shotClock: 10,
+    //  //   endGame: function() {
+    //  //    	$("#gameContainer").html(this.wrong)
+    // 	// $("#gameContainer").text(this.unanswered);
+    // 	// $("#gameContainer").text(this.wins);
+    // };
 
     // functions
     // ------------------------------------------------------------------------------
-    function setGame1() {
+
+    function setGame() {
 
         intervalId = setInterval(function() {
             shotClock--;
             console.log(shotClock);
             $("#gameContainer").html("<div><p>Shot Clock: " + shotClock + "</p></div>");
-            $("#gameContainer").append("<div><p>" + questions.questionOne.question + "</p></div>");
+            $("#gameContainer").append("<div><p>" + questions[0].question + "</p></div>");
+            // clockCheck();
 
             for (var i = 0; i < 4; i++) {
-                var button = $("<button>");
+                button = $("<button>");
                 button.addClass("gameTimeAs");
-                button.attr("data-name", questions.questionOne.answers[i]);
-                button.text(questions.questionOne.answers[i]);
+                button.attr("data-name", questions[0].answers[i]);
+                button.text(questions[0].answers[i]);
                 $("#gameContainer").append(button);
                 clockCheck();
-
-            }
+                
+				   $(button).on("click", ".gameTimeAs", function(){
+				    	console.log("HELLO");
+				    });
+            };
 
         }, 1000);
 
-        function clockCheck() {
-            if (shotClock === 0) {
-                unanswered++;
-                clearInterval(intervalId);
-                $("#gameContainer").html("<div><p>Shot Clock: " + shotClock + "</p></div>");
-                $("#gameContainer").append("<div><p>We call that an AIRBALL!!!</p></div>");
-                $("#gameContainer").append("<img src=" + questions.questionOne.gif + ">");
+};
 
-            }
-        };
 
+
+
+
+    function clockCheck() {
+        if (shotClock === 0) {
+            unanswered += 1;
+            console.log("Unanswered: " + unanswered);
+            clearInterval(intervalId);
+            $("#gameContainer").html("<div><p>Shot Clock: " + shotClock + "</p></div>");
+            $("#gameContainer").append("<div><p>We call that an AIRBALL!!!</p></div>");
+            $("#gameContainer").append("<img src=" + questions[0].gif + ">");
+        }
     };
 
+    function checkAnswer() {
+        if ($(".gameTimeAs").data("name") === questions[0].correctAnswer) {
+            correct++;
+            console.log("Wins: " + correct);
+            clearInterval(intervalId);
+            $("#gameContainer").html("<div><p>Shot Clock: " + shotClock + "</p></div>");
+            $("#gameContainer").append("<div><p>NOW THAT'S A SLAM DUNK!!!</p></div>");
+            $("#gameContainer").append("<img src=" + questions[0].gif + ">");
+        }
 
+        else {
+        	wrong++;
+        	console.log("Losses: " + wrong);
+        	clearInterval(intervalId);
+            $("#gameContainer").html("<div><p>Shot Clock: " + shotClock + "</p></div>");
+            $("#gameContainer").append("<div><p>We call that an AIRBALL!!!</p></div>");
+            $("#gameContainer").append("<img src=" + questions[0].gif + ">");
+        };
+    };
+
+    // $(".gameTimeAs").on("click", function checkAnswer() {
+    //     if ($(".gameTimeAs").data("name") === questions[0].correctAnswer) {
+    //         correct++;
+    //         console.log("Wins: " + correct);
+    //         clearInterval(intervalId);
+    //         $("#gameContainer").html("<div><p>Shot Clock: " + shotClock + "</p></div>");
+    //         $("#gameContainer").append("<div><p>NOW THAT'S A SLAM DUNK!!!</p></div>");
+    //         $("#gameContainer").append("<img src=" + questions[0].gif + ">");
+    //     }
+
+    //     else {
+    //     	wrong++;
+    //     	console.log("Losses: " + wrong);
+    //     	clearInterval(intervalId);
+    //         $("#gameContainer").html("<div><p>Shot Clock: " + shotClock + "</p></div>");
+    //         $("#gameContainer").append("<div><p>We call that an AIRBALL!!!</p></div>");
+    //         $("#gameContainer").append("<img src=" + questions[0].gif + ">");
+    //     }
+    // });
 
 
     // main process
     // ------------------------------------------------------------------------------
 
-
-    // var button = $("<button>");
-    // button.attr("id", "begin");
     button.addClass("begin");
     button.text("TipOff");
     $("#startButton").html(button);
 
     $(".begin").click(function() {
         $(".begin").hide("slow");
-        setGame1();
+        setGame();
     });
 
-
-
-
-
-
+    // $(".gameTimeAs").click(function(){
+    // 	console.log("HELLO");
+    // });
 
 });
